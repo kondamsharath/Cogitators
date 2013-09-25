26a950c69c6ecb14cb9242a67b60eee4bc30051b
kondamsharath <sharath.code@gmail.com>
2013-09-25 23:02:05 +0530

    New changes are going to commited

---
+++ cogitatorsApp/cogitatorsApp/public/javascripts/script.js
@@ -0,0 +1,4 @@
+$(function () {          
+        $('#header-second-cell').addClass('header-ribbon');  
+        $("a[href='whatwedo']").removeClass('header-aElement').addClass('atext-active-color');   
+});
